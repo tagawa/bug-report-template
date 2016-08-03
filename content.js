8d9852e74e15058d586ab20b1d8fbd9210301e14
Daniel Davis <daniel@duckduckgo.com>
2016-08-03 23:17:56 +0900

    Added extra icon and iconmonstr.com credit

--- content.js
+++ content.js
@@ -2,13 +2,8 @@
 
 console.log('content.js');
 
-var template = "## Steps to reproduce:\n1. \n2. \n3. \n\n## What should happen:\n\n## What actually happens:\n";
+var template = "## Steps to reproduce:\n1. \n2. \n3. \n\n## What should happen:\n\n\n## What actually happens:\n";
 
 // Get the text box that's in focus and fill in the bug report template
 var textBox = document.activeElement;
-
-if (textBox.value) {
-    textBox.value = template;
-} else {
-    textBox.innerHTML = template;
-}
+textBox.innerHTML = template;
