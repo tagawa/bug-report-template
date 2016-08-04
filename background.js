ee62ee1ab13614ed71bf0206d0ddf7c687cc11c5
Daniel Davis <daniel@duckduckgo.com>
2016-08-04 23:33:57 +0900

    Removed usage of innerHTML and console messages

--- background.js
+++ background.js
@@ -1,23 +1,14 @@
 'use strict';
 
 /*
-Called when the item has been created, or when creation failed due to an error.
-*/
-function onCreated(n) {
-  if (chrome.runtime.lastError) {
-    console.log("Extension error creating item:" + chrome.runtime.lastError);
-  }
-}
-
-/*
 Create the context menu item.
 */
 chrome.contextMenus.create({
-  id: "add-template",
-  title: chrome.i18n.getMessage("contextMenuLabel"),
-  type: "normal",
-  contexts: ["editable"]
-}, onCreated);
+    id: "add-template",
+    title: chrome.i18n.getMessage("contextMenuLabel"),
+    type: "normal",
+    contexts: ["editable"]
+});
 
 /*
 The click event listener, where we perform the appropriate action given the
@@ -25,10 +16,6 @@ ID of the menu item that was clicked.
 */
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId === 'add-template') {
-      console.log('Adding template');
-
-      chrome.tabs.executeScript(tab.id, {file: "content.js"}, function() {
-          console.log('executeScript: ' + tab.id);
-      });
+      chrome.tabs.executeScript(tab.id, {file: "content.js"});
     }
 });
